d99a3dc75026e3b81aad28ce2ab8f7107d11e9ff
Gabriel Molina <xgmolinax@gmail.com>
2018-03-10 16:08:39 -0600

    React-Intl example component working

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Head from './Header';
 import Nav from './Nav';
-import { FormattedDate } from 'react-intl';
 
 class Layout extends Component {
     render() {
         return (
-          <div>
-              <Head title="Home" />
-              <Nav />
-              {this.props.children}
-              <div>
-                <FormattedDate value={Date.now()} />
-              </div>
-          </div>
+            <div>
+                <Head title="Home" />
+                <Nav />
+                {this.props.children}
+            </div>
         );
     };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
+import { IntlProvider, addLocaleData } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import es from 'react-intl/locale-data/es';
+
 import Layout from '../components/Layout';
 import App from '../components/App';
-import {addLocaleData, IntlProvider} from 'react-intl';
-import enLocaleData from 'react-intl/locale-data/en';
+import messages from '../intl/messages';
+import IntlExample from '../components/examples/IntlExample';
 
-// This example app only uses English. A fake `"en-UPPER"` locale is created so translations can be emulated.
-addLocaleData(enLocaleData);
-addLocaleData({
-    locale: 'en-UPPER',
-    parentLocale: 'en',
-});
+addLocaleData(en);
+addLocaleData(es);
 
-//const {messages} = window.App;
-// messages={messages}
-//locale={navigator.language}
+const language = 'en'; //TODO: Create a redux state for managing language
+
 export default () => (
-  <IntlProvider>
+  <IntlProvider locale={language} messages={messages[language]}>
     <Layout>
-      <App/>
+      <App />
+      <IntlExample />
     </Layout>
   </IntlProvider>
 )
