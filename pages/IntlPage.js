dc56dec59ac150c0028c5a001be0dea894072aec
Gabriel Molina <xgmolinax@gmail.com>
2018-03-14 21:07:31 -0600

    Fixing react intl example

---
+++ pages/IntlPage.js
@@ -0,0 +1,21 @@
+import { IntlProvider, addLocaleData } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import es from 'react-intl/locale-data/es';
+
+import Layout from '../components/Layout';
+import App from '../components/App';
+import messages from '../intl/messages';
+import IntlExample from '../components/examples/IntlExample';
+
+addLocaleData(en);
+addLocaleData(es);
+
+const language = 'en'; //TODO: Create a redux state for managing language
+
+export default () => (
+  <IntlProvider locale={language} messages={messages[language]}>
+    <Layout>
+      <IntlExample />
+    </Layout>
+  </IntlProvider>
+)
