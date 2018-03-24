a6247f398b1d6684b797c08347571c2ccae8f8d9
Gabriel Molina <xgmolinax@gmail.com>
2018-03-24 17:04:57 -0600

    Env setup for API calls with redux actions

--- actions/types.js
+++ actions/types.js
@@ -1,5 +1,11 @@
 export const actionTypes = {
+
+    FETCH_LANGUAGES_REQUEST: 'fetch_language_request',
+    FETCH_LANGUAGES_SUCCESS: 'fetch_language_success',
+    FETCH_LANGUAGES_FAIL: 'fetch_language_fail',
+
     CHANGE_LANGUAGE_REQUEST: 'change_language_request',
     CHANGE_LANGUAGE_SUCCESS: 'change_language_success',
     CHANGE_LANGUAGE_FAIL: 'change_language_fail',
+
 }
--- env.config.js
+++ env.config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? 'https://api.example.com' : 'https://localhost:8080'
+  'process.env.BACKEND_URL': prod ? 'https://api.bitbrackets.io' : 'http://localhost:3001'
 }
