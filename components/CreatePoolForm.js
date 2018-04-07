4ab2546ce0de2ae2edbe4ffd2160221f8bf27dc7
Gabriel Eduardo Molina Pineda <xgmolinax@gmail.com>
2018-04-07 15:38:21 -0600

    Working on forms workflow

--- components/CreatePoolForm.js
+++ components/CreatePoolForm.js
@@ -9,7 +9,12 @@ import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import Divider from 'material-ui/Divider';
 import Checkbox from 'material-ui/Checkbox';
-import { FormGroup, FormControlLabel } from 'material-ui/Form';
+import { FormLabel } from 'material-ui/Form';
+import ExpansionPanel, {
+    ExpansionPanelSummary,
+    ExpansionPanelDetails,
+} from 'material-ui/ExpansionPanel';
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
 
 const styles = theme => ({
     root: {
@@ -39,20 +44,25 @@ const styles = theme => ({
         borderRadius: '5px',
         paddingLeft: theme.spacing.unit * 3,
         paddingRight: theme.spacing.unit * 3,
+        paddingBottom: theme.spacing.unit * 5,
     },
     textField: {
         marginBottom: theme.spacing.unit,
+        width: '60%',
     },
     division: {
         alignSelf: 'center',
-        width: '80%',
+        width: '100%',
         marginTop: theme.spacing.unit * 3,
         marginBottom: theme.spacing.unit * 2,
     },
     nav: {
         marginTop: theme.spacing.unit * 2,
         marginBottom: theme.spacing.unit * 2,
-    }
+    },
+    expansionPanel: {
+        flexGrow: 1,
+    },
 });
 
 class CreatePoolForm extends Component {
@@ -140,7 +150,172 @@ class CreatePoolForm extends Component {
                     />
                     I agree with the pool rules
                 </div>
-                </Form>
+            </Form>
+        );
+    }
+
+    renderReviewDetails() {
+        const { classes } = this.props;
+        const { activeStep } = this.state;
+
+        return (
+            <Form className={classes.formBox}
+                model="pool"
+                onSubmit={(pool) => this.handleSubmit(pool)}
+            >
+                <Typography className={classes.headingTertiary} variant="subheading">
+                    <h4>Admin Info</h4>
+                </Typography>
+                <Control.text
+                    className={classes.textField}
+                    model=".adminName"
+                    component={TextField}
+                    label="Name"
+                    disabled
+                />
+
+                <Control.text
+                    className={classes.textField}
+                    model=".adminEmail"
+                    component={TextField}
+                    label="Email"
+                    disabled
+                />
+                <Divider className={classes.division} />
+
+                <Typography className={classes.headingTertiary} variant="subheading">
+                    <h4>Pool Info</h4>
+                </Typography>
+                <Control.text
+                    className={classes.textField}
+                    model=".poolName"
+                    component={TextField}
+                    label="Name"
+                    disabled
+                />
+
+                <Control.text
+                    className={classes.textField}
+                    model=".entryPassword"
+                    component={TextField}
+                    label="Entry Password"
+                    disabled
+                />
+
+                <Control.text
+                    className={classes.textField}
+                    model=".entryPrice"
+                    component={TextField}
+                    label="Entry Price"
+                    disabled
+                />
+                <Divider className={classes.division} />
+
+                <Typography className={classes.headingTertiary} variant="subheading">
+                    <h4>Payment Details</h4>
+                </Typography>
+                <Control.text
+                    className={classes.textField}
+                    model=".entryPrice"
+                    component={TextField}
+                    label="Entry Price"
+                    disabled
+                />
+
+                <Control.text
+                    className={classes.textField}
+                    model=".entryFee"
+                    component={TextField}
+                    label="Entry Fee"
+                    disabled
+                />
+            </Form>
+        );
+    }
+
+    renderPayment() {
+        const { classes } = this.props;
+        const { activeStep } = this.state;
+
+        return (
+            <Form className={classes.formBox}
+                model="pool"
+                onSubmit={(pool) => this.handleSubmit(pool)}
+            >
+                <Typography className={classes.headingTertiary} variant="subheading">
+                    <h4>Finish Installing Metamesk to continue</h4>
+                </Typography>
+
+                <Typography>
+                    <p>Here some help:</p>
+                </Typography>
+
+                <div className={classes.expansionPanel}>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography>How to Install Metamask</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Typography>
+                                Steps...
+                            </Typography>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography>Getting ether your digital currency</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Typography>
+                                Steps...
+                            </Typography>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography>How to send ETH to MetaMask</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Typography>
+                                Steps...
+                            </Typography>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                </div>
+
+            </Form>
+        );
+    }
+
+    renderPaymentDetails() {
+        const { classes } = this.props;
+        const { activeStep } = this.state;
+
+        return (
+            <Form className={classes.formBox}
+                model="pool"
+                onSubmit={(pool) => this.handleSubmit(pool)}
+            >
+                <Typography className={classes.headingTertiary} variant="subheading">
+                    <h4>Payment Details</h4>
+                </Typography>
+                
+                <Control.text
+                    className={classes.textField}
+                    model=".entryPrice"
+                    component={TextField}
+                    label="Entry Price"
+                    disabled
+                />
+
+                <Control.text
+                    className={classes.textField}
+                    model=".entryFee"
+                    component={TextField}
+                    label="Entry Fee"
+                    disabled
+                />
+            </Form>
         );
     }
 
@@ -168,11 +343,11 @@ class CreatePoolForm extends Component {
             case 1:
                 return this.renderPoolSetup();
             case 2:
-                return '2. Review Details';
+                return this.renderReviewDetails();
             case 3:
-                return '3. Payment';
+                return this.renderPayment();;
             default:
-                return 'Uknown stepIndex';
+                return 'Unknown stepIndex';
         }
     }
 
@@ -210,7 +385,7 @@ class CreatePoolForm extends Component {
                 </div>
                 <div className={classes.headingSecondary}>
                     <Typography variant="title" gutterBottom>
-                        <h4>{`${activeStep}. ${this.getSteps()[activeStep-1]}`}</h4>
+                        <h4>{`${activeStep}. ${this.getSteps()[activeStep - 1]}`}</h4>
                     </Typography>
                 </div>
                 {this.getStepContent(activeStep)}
