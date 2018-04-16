357ad907e2c9771aa9b5a8ba98bdae7f398b5d40
Gabriel Eduardo Molina Pineda <xgmolinax@gmail.com>
2018-04-15 18:35:43 -0600

    Adding redux connect to Layout

--- components/Layout.js
+++ components/Layout.js
@@ -12,6 +12,9 @@ import initMessages from "../intl/";
 
 import Footer from "../components/Footer/Footer";
 
+import LanguageSelect from '../components/LanguageSelect';
+import { changeLanguage, fetchLanguages } from '../actions';
+
 const language = "es"; //TODO: Create a redux state for managing language
 addLocaleData([...en, ...es]);
 const messages = initMessages();
@@ -19,7 +22,16 @@ const messages = initMessages();
 console.log("messages", messages);
 
 class Layout extends Component {
+  componentWillMount() {
+    this.props.fetchLanguages();
+  }
+
   render() {
+    <LanguageSelect
+      languages={this.props.languages}// {["en", "es"]}
+      current={this.props.current}
+      onChange={(value) => this.props.changeLanguage(value)}
+    />
     console.log("esto son los mensajes", messages[language]);
     return (
       <IntlProvider locale={language} messages={messages[language]}>
@@ -36,4 +48,18 @@ class Layout extends Component {
   }
 }
 
-export default Layout;
+function mapStateToProps(state) {
+  return {
+    current: state.language.current,
+    languages: state.language.languages,
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeLanguage: bindActionCreators(changeLanguage, dispatch),
+    fetchLanguages: bindActionCreators(fetchLanguages, dispatch),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Layout);
