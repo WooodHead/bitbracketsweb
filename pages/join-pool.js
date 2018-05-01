5dcf58d1c1fac6f6cfcd5211a215905160d211e6
Gabriel Eduardo Molina Pineda <xgmolinax@gmail.com>
2018-05-01 17:29:06 -0600

    fixing MD HOC placement

--- pages/join-pool.js
+++ pages/join-pool.js
@@ -3,7 +3,7 @@ import { bindActionCreators } from "redux";
 import withRedux from "next-redux-wrapper";
 import Link from "next/link";
 
-import withRoot from '../md/withRoot';
+import withRoot from '../components/HOC/md/withRoot';
 import { initStore } from '../store';
 import JoinPoolLayout from '../components/JoinPoolForm/JoinPoolLayout';
 import Layout from "../components/Layout";
