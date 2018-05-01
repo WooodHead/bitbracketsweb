6446a9c56fef5499178b68b790524c8a7938eb5e
Gabriel Eduardo Molina Pineda <xgmolinax@gmail.com>
2018-05-01 16:17:03 -0600

    Fixed mock action on join

--- actions/joinPoolActions.js
+++ actions/joinPoolActions.js
@@ -4,7 +4,7 @@ const API = process.env.BACKEND_URL;
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-export const joinPool = (pool) => async dispatch => {
+export const joinPool = (pool, prediction) => async dispatch => {
     dispatch({ type: actionTypes.JOIN_POOL_REQUEST });
     await delay(5000);
     dispatch({ type: actionTypes.JOIN_POOL_SUCCESS, payload: pool });
