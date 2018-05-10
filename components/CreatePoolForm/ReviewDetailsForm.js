4932ef86b7f3fc5e04c18adb2945d714175a1b00
Douglas Molina <doug.molina@coralbytes.com>
2018-05-10 15:06:23 -0600

    remove email and admin nickname from pool flow

--- components/CreatePoolForm/ReviewDetailsForm.js
+++ components/CreatePoolForm/ReviewDetailsForm.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import { injectIntl, defineMessages, FormattedMessage } from 'react-intl';
-import { Control, Form, actions } from 'react-redux-form';
+/* eslint-disable react/forbid-prop-types */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { injectIntl, defineMessages } from 'react-intl';
+import { Control, Form } from 'react-redux-form';
 
 import { withStyles } from 'material-ui/styles';
 import TextField from 'material-ui/TextField';
-import Stepper, { Step, StepLabel } from 'material-ui/Stepper';
-import Button from 'material-ui/Button';
+// import Stepper, { Step, StepLabel } from 'material-ui/Stepper';
+// import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import Divider from 'material-ui/Divider';
-import Checkbox from 'material-ui/Checkbox';
-import { FormLabel } from 'material-ui/Form';
+// import Checkbox from 'material-ui/Checkbox';
+// import { FormLabel } from 'material-ui/Form';
 import Grid from 'material-ui/Grid';
 
 const styles = theme => ({
@@ -41,17 +43,20 @@ const messages = defineMessages({
   headingTertiary1: {
     id: 'headingTertiary1',
     defaultMessage: 'Admin Info',
-    description: 'Create Pool Page -> Create Pool Form -> Review Details Form -> tittle 1',
+    description:
+      'Create Pool Page -> Create Pool Form -> Review Details Form -> tittle 1',
   },
   headingTertiary2: {
     id: 'headingTertiary2',
     defaultMessage: 'Pool Info',
-    description: 'Create Pool Page -> Create Pool Form -> Review Details Form -> tittle 2',
+    description:
+      'Create Pool Page -> Create Pool Form -> Review Details Form -> tittle 2',
   },
   headingTertiary3: {
     id: 'headingTertiary3',
     defaultMessage: 'Payment Details',
-    description: 'Create Pool Page -> Create Pool Form -> Review Details Form -> tittle 3',
+    description:
+      'Create Pool Page -> Create Pool Form -> Review Details Form -> tittle 3',
   },
   nameLabel: {
     id: 'nameLabel',
@@ -80,70 +85,63 @@ const messages = defineMessages({
   },
 });
 
-class ReviewDetailForm extends Component {
-  render() {
-    const { classes, intl } = this.props;
+const ReviewDetailForm = ({ classes, intl }) => (
+  <Form className={classes.formBox} model="createPool">
+    {/* <Typography className={classes.headingTertiary} variant="subheading">
+      {intl.formatMessage(messages.headingTertiary1)}
+    </Typography> */}
 
-    return (
-          <Form
-            className={classes.formBox}
-              model="createPool"
-            >
-              <Typography className={classes.headingTertiary} variant="subheading">
-                  {intl.formatMessage(messages.headingTertiary1)}
-                </Typography>
+    {/* <Grid container alignItems="baseline">
+      <Grid item xs={3}>
+        <Typography>{intl.formatMessage(messages.nameLabel)}</Typography>
+      </Grid>
+      <Grid item xs={9}>
+        <Control.text
+          className={classes.textField}
+          model=".adminName"
+          component={TextField}
+          disabled
+          InputProps={{ disableUnderline: true }}
+        />
+      </Grid>
+    </Grid>
 
-              <Grid container alignItems="baseline">
-                  <Grid item xs={3}>
-                      <Typography>{intl.formatMessage(messages.nameLabel)}</Typography>
-                    </Grid>
-                  <Grid item xs={9}>
-                      <Control.text
-                          className={classes.textField}
-                          model=".adminName"
-                          component={TextField}
-                          disabled
-                          InputProps={{ disableUnderline: true }}
-                        />
-                    </Grid>
-                </Grid>
+    <Grid container alignItems="baseline">
+      <Grid item xs={3}>
+        <Typography>{intl.formatMessage(messages.emailLabel)}</Typography>
+      </Grid>
+      <Grid item xs={9}>
+        <Control.text
+          className={classes.textField}
+          model=".adminEmail"
+          component={TextField}
+          disabled
+          InputProps={{ disableUnderline: true }}
+        />
+      </Grid>
+    </Grid>
 
-              <Grid container alignItems="baseline">
-                  <Grid item xs={3}>
-                      <Typography>{intl.formatMessage(messages.emailLabel)}</Typography>
-                    </Grid>
-                  <Grid item xs={9}>
-                      <Control.text
-                          className={classes.textField}
-                          model=".adminEmail"
-                          component={TextField}
-                          disabled
-                          InputProps={{ disableUnderline: true }}
-                        />
-                    </Grid>
-                </Grid>
+    <Divider className={classes.division} /> */}
 
-              <Divider className={classes.division} />
+    <Typography className={classes.headingTertiary} variant="subheading">
+      {intl.formatMessage(messages.headingTertiary2)}
+    </Typography>
+    <Grid container alignItems="baseline">
+      <Grid item xs={3}>
+        <Typography>{intl.formatMessage(messages.nameLabel)}</Typography>
+      </Grid>
+      <Grid item xs={9}>
+        <Control.text
+          className={classes.textField}
+          model=".poolName"
+          component={TextField}
+          disabled
+          InputProps={{ disableUnderline: true }}
+        />
+      </Grid>
+    </Grid>
 
-              <Typography className={classes.headingTertiary} variant="subheading">
-                  {intl.formatMessage(messages.headingTertiary2)}
-                </Typography>
-              <Grid container alignItems="baseline">
-                  <Grid item xs={3}>
-                      <Typography>{intl.formatMessage(messages.nameLabel)}</Typography>
-                    </Grid>
-                  <Grid item xs={9}>
-                      <Control.text
-                          className={classes.textField}
-                          model=".poolName"
-                          component={TextField}
-                          disabled
-                          InputProps={{ disableUnderline: true }}
-                        />
-                    </Grid>
-                </Grid>
-
-              {/* <Grid container alignItems='baseline'>
+    {/* <Grid container alignItems='baseline'>
                     <Grid item xs={3}>
                         <Typography>{intl.formatMessage(messages.entryPasswordLabel)}</Typography>
                     </Grid>
@@ -158,46 +156,49 @@ class ReviewDetailForm extends Component {
                     </Grid>
                 </Grid> */}
 
-              <Grid container alignItems="baseline">
-                  <Grid item xs={3}>
-                      <Typography>{intl.formatMessage(messages.entryLabel)}</Typography>
-                    </Grid>
-                  <Grid item xs={9}>
-                      <Control.text
-                          className={classes.textField}
-                          model=".entryPrice"
-                          component={TextField}
-                          disabled
-                          InputProps={{ disableUnderline: true }}
-                          helperText='ETH'
-                        />
-                    </Grid>
-                </Grid>
+    <Grid container alignItems="baseline">
+      <Grid item xs={3}>
+        <Typography>{intl.formatMessage(messages.entryLabel)}</Typography>
+      </Grid>
+      <Grid item xs={9}>
+        <Control.text
+          className={classes.textField}
+          model=".entryPrice"
+          component={TextField}
+          disabled
+          InputProps={{ disableUnderline: true }}
+          helperText="ETH"
+        />
+      </Grid>
+    </Grid>
 
-              <Divider className={classes.division} />
+    <Divider className={classes.division} />
 
-              <Typography className={classes.headingTertiary} variant="subheading">
-                  {intl.formatMessage(messages.headingTertiary3)}
-                </Typography>
-              <Grid container alignItems="baseline">
-                  <Grid item xs={3}>
-                      <Typography>{intl.formatMessage(messages.feeLabel)}</Typography>
-                    </Grid>
-                  <Grid item xs={9}>
-                      <Control.text
-                          className={classes.textField}
-                          model=".entryFee"
-                          component={TextField}
-                          disabled
-                          InputProps={{ disableUnderline: true }}
-                          helperText='ETH'
-                        />
-                    </Grid>
-              </Grid>
+    <Typography className={classes.headingTertiary} variant="subheading">
+      {intl.formatMessage(messages.headingTertiary3)}
+    </Typography>
+    <Grid container alignItems="baseline">
+      <Grid item xs={3}>
+        <Typography>{intl.formatMessage(messages.feeLabel)}</Typography>
+      </Grid>
+      <Grid item xs={9}>
+        <Control.text
+          className={classes.textField}
+          model=".entryFee"
+          component={TextField}
+          disabled
+          InputProps={{ disableUnderline: true }}
+          helperText="ETH"
+        />
+      </Grid>
+    </Grid>
+  </Form>
+);
 
-          </Form>
-    );
-  }
-}
+ReviewDetailForm.propTypes = {
+  classes: PropTypes.object.isRequired,
+  intl: PropTypes.object.isRequired,
+};
+
 
 export default withStyles(styles)(injectIntl(ReviewDetailForm));
