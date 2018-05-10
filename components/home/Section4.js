8373da58221144594bbe43000a419db92a17d165
Pamela Rivera <pamelarivera@MacBook-Pro-de-Pamela.local>
2018-05-10 12:46:56 -0600

    fixing issues apariencia

--- components/home/Section4.js
+++ components/home/Section4.js
@@ -1,73 +1,101 @@
-import React, { Component } from "react";
-import Typography from "material-ui/Typography";
-import Grid from "material-ui/Grid";
-import { injectIntl, defineMessages, FormattedMessage } from "react-intl";
-import Card, { CardActions, CardContent, CardMedia } from "material-ui/Card";
+/* eslint-disable react/forbid-prop-types */
+
+import React from 'react';
+import Typography from 'material-ui/Typography';
+import PropTypes from 'prop-types';
+import Grid from 'material-ui/Grid';
+import { injectIntl, defineMessages } from 'react-intl';
+import Paper from 'material-ui/Paper';
+
+import { withStyles } from 'material-ui/styles';
 
 const messages = defineMessages({
   homeSection4Title1: {
-    id: "home.section4.title1",
-    defaultMessage: "Claim the prize",
-    description: "home > section3"
+    id: 'home.section4.title1',
+    defaultMessage: 'Claim the prize',
+    description: 'home > section3',
   },
   homeSection4Title2: {
-    id: "home.section4.title2",
-    defaultMessage: "The Balance of the pool goes to the winner",
-    description: "home > section3"
+    id: 'home.section4.title2',
+    defaultMessage: 'The Balance of the pool goes to the winner',
+    description: 'home > section3',
   },
   homeSection4Title3: {
-    id: "home.section4.title3",
-    defaultMessage: "If you are the pool creator you will win a ",
-    description: "home > section3"
+    id: 'home.section4.title3',
+    defaultMessage: 'If you are the pool creator you will win a ',
+    description: 'home > section3',
   },
   homeSection4Title4: {
-    id: "home.section4.title4",
-    defaultMessage: "fee of the pool for helping to spread crypto awareness",
-    description: "home > section3"
-  }
+    id: 'home.section4.title4',
+    defaultMessage: 'fee of the pool for helping to spread crypto awareness',
+    description: 'home > section3',
+  },
 });
 
-class Section4 extends Component {
-  render() {
-    const { intl } = this.props;
-    return (
-      <div style={{ marginTop: "2em", marginBottom: "2em", marginLeft: "1em" }}>
-        <div className="container">
-          <Grid container spacing={24}>
-            <Grid item xs={12} sm={6} align="center">
-              <img src="../../static/img4.png" width="200" height="200" />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Typography
-                variant="display2"
-                align="justify"
-                gutterBottom
-                style={{ color: "black", fontWeight: "500" }}
-              >
-                {intl.formatMessage(messages.homeSection4Title1)}
-              </Typography>
-              <Typography
-                variant="headline"
-                align="justify"
-                style={{
-                  color: "black",
-                  fontWeight: "400",
-                  letterSpacing: "0.5px"
-                }}
-              >
-                {intl.formatMessage(messages.homeSection4Title2)}
-                <br />
-                {intl.formatMessage(messages.homeSection4Title3)}
 
-                <span style={{ color: "#E72459" }}>%</span>
-                {intl.formatMessage(messages.homeSection4Title4)}
-              </Typography>
-            </Grid>
-          </Grid>
-        </div>
-      </div>
-    );
-  }
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+function Section4(props) {
+  const { intl, classes } = props;
+  return (
+
+    <div className={classes.root} style={{ marginTop: '6em', marginBottom: '6em', marginLeft: '1em' }}>
+      <Grid container spacing={24}>
+
+        <Grid item xs={12} sm={6} align="center">
+          <span className={classes.paper}>
+            <img src="../../static/img4.png" width="200" height="200" alt="crypto" />
+          </span>
+
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <div className={classes.paper}>
+            <Typography
+
+              variant="display2"
+              align="justify"
+              gutterBottom
+              style={{ color: 'black', fontWeight: '500' }}
+            >
+              {intl.formatMessage(messages.homeSection4Title1)}
+            </Typography>
+            <Typography
+              classesName={classes.paper}
+              variant="headline"
+              align="justify"
+              style={{
+                  color: 'black',
+                  fontWeight: '400',
+                  letterSpacing: '0.5px',
+                }}
+            >
+              {intl.formatMessage(messages.homeSection4Title2)}
+              <br />
+              {intl.formatMessage(messages.homeSection4Title3)}
+
+
+              {intl.formatMessage(messages.homeSection4Title4)}
+            </Typography>
+          </div>
+        </Grid>
+
+
+      </Grid>
+    </div>
+  );
 }
 
-export default injectIntl(Section4);
+Section4.propTypes = {
+  classes: PropTypes.object.isRequired,
+  intl: PropTypes.object.isRequired,
+};
+
+export default injectIntl(withStyles(styles)(Section4));
--- reducers/languageReducer.js
+++ reducers/languageReducer.js
@@ -2,7 +2,7 @@ import { actionTypes } from "../actions/types";
 
 const INITIAL_STATE = {
   languages: [],
-  current: "en",
+  current: "es",
   loading: false,
   error: undefined
 };
