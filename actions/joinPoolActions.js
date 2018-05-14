a38b6e759ff2154433341662de3c5e15aa4ae6d3
Douglas Molina <doug.molina@coralbytes.com>
2018-05-13 18:36:09 -0600

    add payment tx to pool send prediction

--- actions/joinPoolActions.js
+++ actions/joinPoolActions.js
@@ -1,15 +1,32 @@
+/* eslint-disable import/prefer-default-export */
 import _ from 'lodash';
 import { actionTypes } from '../actions/types';
+import getContestPoolInstance from '../ethereum/contestPool';
+import web3 from '../ethereum/web3';
 
-const API = process.env.BACKEND_URL;
-
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 const isNotUndefined = value => !(_.isUndefined(value));
 
-export const joinPool = (pool, predictions) => async dispatch => {
-    dispatch({ type: actionTypes.JOIN_POOL_REQUEST });
-    const predictionArray = _.map(predictions, 'prediction').filter(isNotUndefined);
-    console.log('Predictions: ', predictionArray);
-    await delay(5000);
-    dispatch({ type: actionTypes.JOIN_POOL_SUCCESS, payload: predictions });
-}
+export const joinPool = (pool, predictions) => dispatch =>
+  new Promise(async (resolve, reject) => {
+    try {
+      dispatch({ type: actionTypes.JOIN_POOL_REQUEST });
+      if (!pool.address) {
+        throw new Error('System Error: Expected a valid address');
+      }
+      console.log('Predictions: ', predictions);
+      const poolInstance = await getContestPoolInstance(pool.address);
+      const accounts = await web3.eth.getAccounts();
+      const tx = await poolInstance.methods
+        .sendPredictionSet(predictions)
+        .send({
+          from: accounts[0],
+          value: pool.amountPerPlayer,
+        });
+      dispatch({ type: actionTypes.JOIN_POOL_SUCCESS, payload: tx });
+      resolve(tx);
+    } catch (error) {
+      dispatch({ type: actionTypes.JOIN_POOL_FAIL, payload: error });
+      reject(error);
+    }
+  });
+
