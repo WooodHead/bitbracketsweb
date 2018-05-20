c583756c8ee627965e2551ba183c2e9151db0c44
Pamela Rivera <pamelarivera@MacBook-Pro-de-Pamela.local>
2018-05-20 17:03:20 -0600

    fixing social

--- components/Header.js
+++ components/Header.js
@@ -20,18 +20,18 @@ const Header = props => (
     <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
     <link rel="icon" href="/static/favicon.ico" />
     <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" />
-    <meta property="og:url" content={props.url || defaultOGURL} />
+    {/* <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:title" content={props.title || ''} />
     <meta
       property="og:description"
       content={props.description || defaultDescription}
-    />
+    /> */}
     <meta name="twitter:site" content={props.url || defaultOGURL} />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image" content={props.ogImage || defaultOGImage} />
+    {/* <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
+    <meta property="og:image:height" content="630" /> */}
   </NextHead>
 );
 
