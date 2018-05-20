963f0a2590aad728f1c3da5363412af9842c9544
dtutila <daniel.tutila@gmail.com>
2018-05-20 14:33:36 -0600

    updating api url

--- env.config.js
+++ env.config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? 'https://api.bitbrackets.io' : 'http://localhost:3001'
+  'process.env.BACKEND_URL': prod ? 'https://rinkeby.api.bitbrackets.io' : 'http://localhost:3001'
 }
