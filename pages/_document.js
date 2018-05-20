c862dfa27312273718924f09de9efad586908da7
Pamela Rivera <pamelarivera@MacBook-Pro-de-Pamela.local>
2018-05-20 15:32:58 -0600

    MetaMaskLockedui

--- pages/_document.js
+++ pages/_document.js
@@ -38,6 +38,7 @@ class MyDocument extends Document {
         
 
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
 
         </Head>
         <body>
