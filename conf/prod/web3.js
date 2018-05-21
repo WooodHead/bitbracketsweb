6a2408fa1f0c968a587696a106d0197129fb5eb5
Douglas Molina <doug.molina@coralbytes.com>
2018-05-20 21:54:52 -0600

    hardcode env prod vars

--- conf/prod/web3.js
+++ conf/prod/web3.js
@@ -2,9 +2,9 @@
 const WEB3 = {
   networks: ['main', 'ropsten', 'kovan', 'rinkeby'],
   interval: 1000,
-  factoryAddress: process.env.FACTORY_ADDR,
-  infuraUrl: process.env.INFURA_KEY,
-  contestName: process.env.CONTEST_NAME,
+  factoryAddress: '0x2230747607ae54002a0a33da54511d54014c317e' || process.env.FACTORY_ADDR,
+  infuraUrl: 'https://rinkeby.infura.io/x8dGVFhCSTJoQ7WJ4OIP' || process.env.INFURA_KEY,
+  contestName: 'Russia2018Test' || process.env.CONTEST_NAME,
 };
 
 module.exports = WEB3;
