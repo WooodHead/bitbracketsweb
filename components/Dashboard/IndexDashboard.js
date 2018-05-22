03363f95fa15f93ab2b8ff1ab9415b75027246cb
Pamela Rivera <pamelarivera@MacBook-Pro-de-Pamela.local>
2018-05-22 09:30:24 -0600

    prediction success label

--- components/Dashboard/IndexDashboard.js
+++ components/Dashboard/IndexDashboard.js
@@ -4,10 +4,22 @@ import React from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
+import { injectIntl, defineMessages } from 'react-intl';
+import Typography from 'material-ui/Typography';
 
 import TimeRemaining from './TimeRemaining';
 import PoolDetails from './PoolDetails';
 import ParticipantList from './ParticipantList';
+import Messages from './Messages';
+
+const messages = defineMessages({
+  predictionSuccessMessage: {
+    id: 'predictionSuccessMessage',
+    defaultMessage: 'Your prediction has been successfully processed. Please give it refresh if it is not listed yet.',
+    description: 'predictionSuccessMessage',
+  },
+});
+
 
 const styles = theme => ({
   root: {
@@ -23,20 +35,33 @@ const styles = theme => ({
 });
 
 const IndexDashboard = (props) => {
-  const { pool, classes } = props;
+  const {
+    pool, classes, predictionSuccess, intl,
+  } = props;
   const dateStartTime = moment.unix(pool.startTime).toISOString();
+  const msg = (
+    <Typography variant="subheading" style={{ color: 'green' }}>
+      {intl.formatMessage(messages.predictionSuccessMessage)}
+    </Typography>);
   return (
     <div className={classes.root}>
+      <Messages messages={predictionSuccess ? msg : undefined} />
       <TimeRemaining date={dateStartTime} />
       <PoolDetails pool={pool} />
       <ParticipantList pool={pool} />
-  
+
     </div>
   );
 };
 
+IndexDashboard.defaultProps = {
+
+  predictionSuccess: undefined,
+};
 IndexDashboard.propTypes = {
   pool: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired,
+  predictionSuccess: PropTypes.string,
+  intl: PropTypes.object.isRequired,
 };
-export default withStyles(styles)(IndexDashboard);
+export default injectIntl(withStyles(styles)(IndexDashboard));
