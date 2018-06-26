042ab0381b4535bd0b1ff74263c000bb11519b72
Oscar Calderon <oscar@calderon.cn>
2018-06-25 21:46:42 -0700

    Merge de conflictos

--- actions/poolsActions.js
+++ actions/poolsActions.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 import { actionTypes } from '../actions/types';
 
  const BASE_URL = process.env.ENDPOINT_URL;
- // const API = 'http://localhost:3001/users/managerA/pools';
-  // const API = 'https://rinkeby.api.bitbrackets.io/api';
-  const API = BASE_URL;
 
 export const fetchPools = (address) => async dispatch => {
   dispatch({ type: actionTypes.FETCH_POOLS_REQUEST });
