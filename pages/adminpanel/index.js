f7f12e9f340a57a072d3f87f51f9a01c5171c6f7
Douglas Molina <doug.molina@coralbytes.com>
2018-09-01 17:44:45 -0600

    add react admin framework

---
+++ pages/adminpanel/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/forbid-prop-types */
+import React from 'react';
+// import PropTypes from 'prop-types';
+import AdminDashboard from '../../containers/admindashboard';
+
+const AdminPanelPage = () => <AdminDashboard />;
+
+export default AdminPanelPage;
