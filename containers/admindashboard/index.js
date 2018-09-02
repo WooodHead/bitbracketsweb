9154a2d58d3a7e0ed057c4c18a8f6caea6036eff
Douglas Molina <doug.molina@coralbytes.com>
2018-09-01 19:48:35 -0600

    adds contest definition list to admin dashboard

--- containers/admindashboard/index.js
+++ containers/admindashboard/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import createHistory from 'history/createMemoryHistory';
+import ContestList from './contestList';
 
-const dataProvider = jsonServerProvider('process.env.ENDPOINT_URL');
+const dataProvider = jsonServerProvider(process.env.ENDPOINT_URL);
 
-const AdminDashboard = () => <Admin dataProvider={dataProvider} />;
+const history = createHistory();
+
+const AdminDashboard = () => (
+    <Admin history={history} dataProvider={dataProvider} >
+        <Resource name="contests" list={ContestList} />
+    </Admin>
+);
 
 export default AdminDashboard;
