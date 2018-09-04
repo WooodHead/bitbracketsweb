ec47bf6c5cdd23c7819c90b2f9288bb6bd6de40e
Douglas Molina <doug.molina@coralbytes.com>
2018-09-03 18:02:05 -0600

    refactoring directories

--- pages/adminpanel/index.js
+++ pages/adminpanel/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
 // import PropTypes from 'prop-types';
-import AdminDashboard from '../../containers/adminDashboard';
+import AdminDashboard from '../../containers/admindashboard';
 
 const AdminPanelPage = () => <AdminDashboard />;
 
