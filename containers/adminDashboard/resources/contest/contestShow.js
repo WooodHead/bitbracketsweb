e03e2dc928dcabcbce7b03a414694a09adf6b891
Douglas Molina <doug.molina@coralbytes.com>
2018-09-03 18:22:51 -0600

    fix build

--- containers/adminDashboard/resources/contest/contestShow.js
+++ containers/adminDashboard/resources/contest/contestShow.js
@@ -4,13 +4,13 @@ import { Show, SimpleShowLayout, TextField, DateField } from 'react-admin';
 const ContestShow = props => (
   <Show {...props}>
     <SimpleShowLayout>
-      <TextField label="Contest Name" source="contestName" />
+      {/* <TextField label="Contest Name" source="contestName" />
       <DateField label="Start Time" source="startTime" />
       <DateField label="End Time" source="endTime" defaultValue={new Date()} />
       <TextField label="Grace Time in Hours" source="graceTime" />
       <TextField label="Max Balance in ETH" source="maxBalance" />
       <TextField label="Manager Fee %" source="managerFee" />
-      <TextField label="Platform Fee %" source="platformFee" />
+      <TextField label="Platform Fee %" source="platformFee" /> */}
     </SimpleShowLayout>
   </Show>
 );
