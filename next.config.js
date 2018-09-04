465920b96da3fe83f90b3763cd27704f73ce20b0
Douglas Molina <doug.molina@coralbytes.com>
2018-09-04 13:55:23 -0600

    modify webpack

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-require('dotenv').config();
+const { parsed: localEnv, error } = require('dotenv').config();
+const webpack = require('webpack');
+
+if (error) {
+  throw error;
+}
 
 module.exports = {
   webpack: (config, { dev }) => {
@@ -6,6 +11,10 @@ module.exports = {
     config.node = {
       fs: 'empty'
     };
+
+    console.log('env', localEnv);
+
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 
     // Perform customizations to config
     config.module.rules = config.module.rules.map(rule => {
