d8ec171bc7839b1ab756c02a0b95a2423649ef48
Douglas Molina <doug.molina@coralbytes.com>
2018-09-04 00:03:54 -0600

    renaming component directory

--- pages/adminpanel/index.js
+++ pages/adminpanel/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
 // import PropTypes from 'prop-types';
-import AdminDashboard from '../../components/AdminDashboard';
+import AdminDashboard from '../../components/AdminPanel';
 
 const AdminPanelPage = () => <AdminDashboard />;
 
